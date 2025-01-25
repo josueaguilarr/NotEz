328f2a6b2e815d88799ff9a819f47cb1ccb8ed8f
josueaguilarr <josue.aguilar225@gmail.com>
2025-01-24 22:49:23 -0500

    fix: Removing auth0 for sessions

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -1,16 +1,15 @@
-import { useAuth0 } from "@auth0/auth0-react";
 import { LoginButton } from "./LoginButton";
 import { LogoutButton } from "./LogoutButton";
 import { Title } from "./Title";
 
 export const Header: React.FC = () => {
-  const { isAuthenticated } = useAuth0();
 
   return (
     <header className="flex items-center justify-between mb-16">
       <Title />
 
-      {isAuthenticated ? (<LogoutButton />) : (<LoginButton />)}      
+      <LoginButton />     
+      <LogoutButton />     
     </header>
   );
 };
