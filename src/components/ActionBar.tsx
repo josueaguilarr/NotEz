c6844d85dea4236607663f50006994fdf1712856
josueaguilarr <josue.aguilar225@gmail.com>
2025-02-07 16:45:12 -0500

    fix: improve structure ActionBar

--- src/components/ActionBar.tsx
+++ src/components/ActionBar.tsx
@@ -46,10 +46,10 @@ export const ActionBar: React.FC<Props> = ({
   };
 
   const handleRemoveGroup = () => {
-    if(currentGroupUuid === undefined) return;
+    if (currentGroupUuid === undefined) return;
 
     removeGroup({ uuid: currentGroupUuid });
-  }
+  };
 
   useEffect(() => {
     setTitleGroup(currentGroupName);
@@ -57,27 +57,25 @@ export const ActionBar: React.FC<Props> = ({
 
   return (
     <footer className="flex justify-between gap-7 sm:flex-row flex-col mt-4 mb-6">
-      {activeCount > 0 && (
-        <>
-          <span className="max-w-1/4 truncate flex items-center gap-[2px]">
-            <span>
-              <strong>{activeCount}</strong> {activeTaskWord} pendiente
-              {!singleActiveCount && "s"}{" "}
-              {currentGroupName !== undefined ? "en" : ""}
-              <input
-                ref={inputTitle}
-                type="text"
-                value={titleGroup || ""}
-                onChange={(e) => {
-                  setTitleGroup(e.target.value);
-                }}
-                onBlur={handleUpdateTitleGroup}
-                className="ms-1 text-lg bg-transparent focus:outline-none font-bold gradientText"
-              />
-            </span>
+      <>
+        <span className="max-w-1/4 truncate flex items-center gap-[2px]">
+          <span>
+            <strong>{activeCount}</strong> {activeTaskWord} pendiente
+            {!singleActiveCount && "s"}{" "}
+            {currentGroupName !== undefined ? "en" : ""}
+            <input
+              ref={inputTitle}
+              type="text"
+              value={titleGroup || ""}
+              onChange={(e) => {
+                setTitleGroup(e.target.value);
+              }}
+              onBlur={handleUpdateTitleGroup}
+              className="ms-1 text-lg bg-transparent focus:outline-none font-bold gradientText"
+            />
           </span>
-        </>
-      )}
+        </span>
+      </>
 
       <div className="flex flex-wrap text-xs gap-1 sm:text-base">
         {activeCount > 0 && (
@@ -97,13 +95,11 @@ export const ActionBar: React.FC<Props> = ({
           </button>
         )}
 
-        {activeCount > 0 && (
-          <ActionsGroup
-            groupSelected={currentGroupName}
-            focusInputTitle={() => inputTitle.current?.focus()}
-            handleRemoveGroup={handleRemoveGroup}
-          />
-        )}
+        <ActionsGroup
+          groupSelected={currentGroupName}
+          focusInputTitle={() => inputTitle.current?.focus()}
+          handleRemoveGroup={handleRemoveGroup}
+        />
       </div>
     </footer>
   );
